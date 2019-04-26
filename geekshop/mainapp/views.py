57069e64d79a78f965173949ade82486b08c594c
KSolovjev <KSolovjev@mail.ru>
2019-04-26 17:03:08 +1100

    homework 2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,11 +3,19 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+links_menu = [{'href': 'products_all', 'name': 'all'},
+{'href': 'products_home', 'name': 'home'},
+{'href': 'products_office', 'name': 'office'},
+{'href': 'products_modern', 'name': 'modern'},
+{'href': 'products_classic', 'name': 'classic'}]
+
+context = {'name': 'ivan', 'array': [1, 2, 3, 4, 5]}
+
 def main(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    return render(request, 'mainapp/catalog.html')
+    return render(request, 'mainapp/products.html', links_menu)
 
 def contacts(request):
-    return render(request, 'mainapp/contacts.html')
+    return render(request, 'mainapp/contacts.html', context)
